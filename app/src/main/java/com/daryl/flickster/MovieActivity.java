f6d1a50fa4d6b20e7e8dd83f558259e304587ca3
dhalima3 <dhalima@gatech.edu>
2016-07-23 19:50:56 -0400

    feature- add pull-to-refresh

--- app/src/main/java/com/daryl/flickster/MovieActivity.java
+++ app/src/main/java/com/daryl/flickster/MovieActivity.java
@@ -1,7 +1,8 @@
 package com.daryl.flickster;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ListView;
 
@@ -20,9 +21,13 @@ import cz.msebera.android.httpclient.Header;
 
 public class MovieActivity extends AppCompatActivity {
 
-    ArrayList<Movie> movies;
-    MovieArrayAdapter movieAdapter;
-    ListView lvItems;
+    public static final String MOVIE_API_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+    private ArrayList<Movie> movies;
+    private MovieArrayAdapter movieAdapter;
+    private ListView lvItems;
+    private SwipeRefreshLayout swipeMovieContainer;
+    private AsyncHttpClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +39,28 @@ public class MovieActivity extends AppCompatActivity {
         movieAdapter = new MovieArrayAdapter(this, movies);
         lvItems.setAdapter(movieAdapter);
 
-        String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+        client = new AsyncHttpClient();
+        initializeSwipeMovieContainer();
 
-        AsyncHttpClient client = new AsyncHttpClient();
+        getMovieJson(MOVIE_API_URL, client);
+    }
 
+    protected void initializeSwipeMovieContainer() {
+        swipeMovieContainer = (SwipeRefreshLayout) findViewById(R.id.swipeMovieContainer);
+        swipeMovieContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getMovieJson(MOVIE_API_URL, client);
+                swipeMovieContainer.setRefreshing(false);
+            }
+        });
+        swipeMovieContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+    }
+
+    private void getMovieJson(String url, AsyncHttpClient client) {
         client.get(url, new JsonHttpResponseHandler(){
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
