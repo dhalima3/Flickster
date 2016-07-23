acd3fde26ea6004f54265482f9ac46bea4e3e4ab
dhalima3 <dhalima@gatech.edu>
2016-07-23 16:23:47 -0400

    Initial commit

---
+++ app/src/main/java/com/daryl/flickster/adapters/MovieArrayAdapter.java
@@ -0,0 +1,45 @@
+package com.daryl.flickster.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.daryl.flickster.R;
+import com.daryl.flickster.models.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class MovieArrayAdapter extends ArrayAdapter<Movie> {
+
+    public MovieArrayAdapter(Context context, List<Movie> movies) {
+        super(context, android.R.layout.simple_list_item_1, movies);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Movie movie = getItem(position);
+
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_movie, parent, false);
+        }
+
+        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+        ivImage.setImageResource(0);
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvMovieTitle);
+        TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+
+        tvTitle.setText(movie.getOriginalTitle());
+        tvOverview.setText(movie.getOverview());
+
+        Picasso.with(getContext()).load(movie.getPosterPath()).into(ivImage);
+
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/daryl/flickster/models/Movie.java
@@ -0,0 +1,45 @@
+package com.daryl.flickster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class Movie {
+
+    String posterPath;
+    String originalTitle;
+    String overview;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        this.posterPath = jsonObject.getString("poster_path");
+        this.originalTitle = jsonObject.getString("original_title");
+        this.overview = jsonObject.getString("overview");
+    }
+
+    public static ArrayList<Movie> fromJSONArray(JSONArray jsonArray) {
+        ArrayList<Movie> results = new ArrayList<>();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            try {
+                results.add(new Movie(jsonArray.getJSONObject(i)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return results;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
